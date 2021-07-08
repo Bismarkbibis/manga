1153a8a0de1dbc15ce49b9be3e2e65d885ffb36b
Bismarkbibis <74538779+Bismarkbibis@users.noreply.github.com>
2021-07-08 08:53:50 +0200

    final

--- src/main/java/manga/repository/AdressRepository.java
+++ src/main/java/manga/repository/AdressRepository.java
@@ -1,9 +1,10 @@
 package manga.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import manga.model.Adresse;
-
+@Repository
 public interface AdressRepository extends JpaRepository<Adresse, Integer> {
 
 }
